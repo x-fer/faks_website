56e21c6a88a408af6f00ff3f8bf394be75d63d65
cyber3x <neven.lukic8@gmail.com>
2025-03-11 16:21:19 +0100

    desktop raspored done, added react, and tailwind merge

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://faks.xfer.hr",
-    vite: {
-        plugins: [tailwindcss()],
-    },
+  site: "https://faks.xfer.hr",
+
+  vite: {
+      plugins: [tailwindcss()],
+  },
+
+  integrations: [react()],
 });
---
+++ src/components/raspored/TimeSlotEntry.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@src/assets/lib/utils";
+import type { EventInfo } from "../RasporedPage.astro";
+
+type Props = {
+    event: EventInfo
+    style: {},
+}
+
+export default function TimeSlotEntry({ event, style }: Props) {
+    return (
+        <div className={cn(
+            "rounded-xl absolute px-4 py-3 border-2 md:border-3 inset-shadow-[0_0_50px_5px] flex flex-col justify-between",
+            event.type === "tech" && "left-5 w-5/12 bg-[#360E12] border-[#711B38] inset-shadow-red-500/15",
+            event.type === "policy" && "right-5 w-5/12 bg-[#0C142F] border-[#017EFE] inset-shadow-blue-600/15",
+            event.type === "keynote" && "inset-x-5 bg-[#1D1025] border-[#752C7B] inset-shadow-purple-600/15",
+            event.type === "neutral" && "inset-x-5 bg-[#113711] border-[#0C8A19] inset-shadow-green-600/15")}
+            style={{ ...style }}
+        >
+            <p className="font-open-sans text-md md:text-lg text-white-smoke">{event.title}</p>
+            {event.speaker &&
+            <div className="flex">
+                <p className="font-open-sans uppercase text-xs md:text-md text-gray-400 font-semibold">{event.speaker}</p>
+            </div>
+            }
+        </div>
+    )
+}
+
