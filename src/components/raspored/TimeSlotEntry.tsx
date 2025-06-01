4ca84d56133fa7d307350e9283b85017f0020fc9
atom <atom@pop-os.localdomain>
2025-06-01 19:23:55 +0200

    subtitle

--- src/components/raspored/TimeSlotEntry.tsx
+++ src/components/raspored/TimeSlotEntry.tsx
@@ -7,7 +7,6 @@ type Props = {
   style: React.CSSProperties;
 };
 
-// Define a mapping of event types to their corresponding styles
 const eventStyles = {
   tech: {
     outerDiv:
@@ -39,7 +38,6 @@ const eventStyles = {
   },
 };
 
-// Function to get styles based on event type
 const getEventStyles = (event: EventInfo) => {
   if (!event.isTalk) return eventStyles["neutral"];
   return eventStyles[event.talkInfo.type];
@@ -48,35 +46,48 @@ const getEventStyles = (event: EventInfo) => {
 export default function TimeSlotEntry({ event, style }: Props) {
   const { outerDiv: outerDivStyle, icon } = getEventStyles(event);
 
-  // Helper function to format speakers names
   const formatSpeakers = (
-    speakers?: Array<{ name: string; title?: string }>,
+    speakers?: Array<{ name: string; title?: string }>
   ) => {
     if (!speakers || speakers.length === 0) return "UNKNOWN";
     if (speakers.length === 1) return speakers[0].name;
     return speakers.map((speaker) => speaker.name).join(", ");
   };
 
-  const link = `/predavaci/#${event.talkInfo?.title.toLocaleLowerCase().replaceAll(" ", "-")}`;
+  const link = `/predavaci/#${event.talkInfo?.title
+    .toLocaleLowerCase()
+    .replaceAll(" ", "-")}`;
 
   const content = (
     <div
       className={cn(
-        "absolute flex flex-col justify-between overflow-hidden rounded-md border-2 px-3 py-2 inset-shadow-[0_0_50px_5px] md:border-3 md:px-4 md:py-2",
+        "absolute flex items-start flex-col overflow-hidden rounded-md border-2 px-3 py-2 inset-shadow-[0_0_50px_5px] md:border-3 md:px-4 md:py-2",
         outerDivStyle,
         event.small && "min-h-[28px] rounded-sm",
-        event.isTalk && "cursor-pointer",
+        event.isTalk ? "justify-between cursor-pointer" : "justify-start"
       )}
       style={{ ...style }}
     >
       <p
         className={cn(
           "font-open-sans text-white-smoke line-clamp-3 text-xs font-medium sm:text-base",
-          event.small && "line-clamp-1 text-gray-300 italic",
+          event.small && "line-clamp-1 text-gray-300 italic"
         )}
       >
         {event.isTalk ? event.talkInfo.title : event.title}
       </p>
+
+      {!event.isTalk && event.subtitle && (
+        <p
+          className={cn(
+            "font-open-sans italic text-[15px] leading-tight text-gray-400",
+            event.small ? "line-clamp-1" : "sm:text-xs"
+          )}
+        >
+          {event.subtitle}
+        </p>
+      )}
+
       {event.isTalk && !event.small && (
         <div className="flex items-center gap-2">
           <User className="size-3 flex-shrink-0 md:size-4" color={icon.color} />
